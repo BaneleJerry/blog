d241aaa2f9293c2a4e0d2fd014c6587d5ba1a609
Banele <banelethabede@outlook.com>
2023-07-01 02:04:05 +0200

    ADDED VIEWS

--- blog/tests.py
+++ blog/tests.py
@@ -29,7 +29,7 @@ class BlogTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_url_exists_at_correct_location_detailview(self): # new
-        response = self.client.get("/post/1")
+        response = self.client.get("/post/1/")
         self.assertEqual(response.status_code, 200)
 
     def test_post_listview(self): # new
@@ -42,3 +42,33 @@ class BlogTests(TestCase):
         response = self.client.get(reverse("post_detail",
         kwargs={"pk": self.post.pk}))
         no_response = self.client.get("/post/100000/")
+
+    def test_post_createview(self): # new
+        response = self.client.post(
+        reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+            },
+
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+
+    def test_post_updateview(self): # new
+        response = self.client.post(
+        reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_deleteview(self): # new
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
