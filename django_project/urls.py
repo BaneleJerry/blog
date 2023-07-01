2354bb304863b01a266c809e69ad6445be5ff78b
Banele <banelethabede@outlook.com>
2023-07-01 04:59:08 +0200

    added auth pages and created accounts app

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,5 +19,7 @@ from django.contrib import admin
 from django.urls import path, include # new
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/",include('django.contrib.auth.urls')),
+    path("accounts/", include('accounts.urls')),
     path("", include("blog.urls")), # new
 ]
